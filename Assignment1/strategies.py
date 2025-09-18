a203a758801b41e8af3da150fc88366f3c0e06ad
Rita-Cplusplus <3156835551@qq.com>
2025-09-19 01:10:08 +0800

    Create strategies.py
    Updated Strategy

---
+++ Assignment1/strategies.py
@@ -0,0 +1,55 @@
+# data_generator.py
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+import datetime
+import random
+import time
+import csv
+
+class Strategy(ABC):
+    @abstractmethod
+    def generate_signals(self, tick):  #tick: MarketDataPoint
+        pass
+
+
+class Moving_average_crossover(Strategy);
+    def __init__(self, short_window=3, long_window=5, quantity=10):
+        self.__short_windows = short_window
+        self.__long_windows = long_window
+        self.__quantity = quantity
+        self.__prices=[]
+        
+    def generate_signals(self, tick):  #tick: MarketDataPoint
+        self.__prices.append(tick.price)
+        if len(self.__prices) < self._long_window:
+            return []
+        
+        short_ma = sum(self.__prices[-self.__short_window:]) / self.__short_window
+        long_ma = sum(self.__prices[-self.__long_window:]) / self.__long_window
+
+        if short_ma > long_ma:
+            return [("BUY", tick.symbol, self.__quantity, tick.price)]
+        elif short_ma < long_ma:
+            return [("SELL", tick.symbol, self.__quantity, tick.price)]
+        else:
+            return []
+
+class MomentumStrategy(Strategy):
+    def __init__(self, lookback=3, quantity=10):
+        self.__lookback = lookback
+        self.__quantity = quantity
+        self.__prices = []
+
+    def generate_signals(self, tick: MarketDataPoint):
+        self._prices.append(tick.price)
+        if len(self.__prices) <= self.__lookback:
+            return []
+
+        momentum = tick.price - self.__prices[-self.__lookback]
+
+        if momentum > 0:
+            return [("BUY", tick.symbol, self.__quantity, tick.price)]
+        elif momentum < 0:
+            return [("SELL", tick.symbol, self.__quantity, tick.price)]
+        else:
+            return []
