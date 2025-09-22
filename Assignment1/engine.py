5969af7986a2fc23857e70f6ff0fd2cbcc55b515
Catherine <cat33chen@gmail.com>
2025-09-21 19:54:31 -0500

    Update engine.py
    1. Added .cash for the engine to mark down cash left
    2. for portfolio, document price * volume

--- Assignment1/engine.py
+++ Assignment1/engine.py
@@ -4,12 +4,14 @@ import strategies
 import datetime
 import time
 import csv
+import random
 
 class ExecutionEngine:
-    def __init__(self, market_data, strategies):
+    def __init__(self, market_data, strategies, starting_cash):
         # Iterate through the list of MarketDataPoint objects in timestamp order.
         self.market_data = sorted(market_data, key=lambda x: x.timestamp)
         self.strategies = strategies
+        self.cash = starting_cash
         self.portfolio = {}
         #In the execution engine, simulate occasional failures and raise ExecutionError; catch and log these errors to continue processing.
         self.execution_failure_chance = 0.05  #Assuming fail rate
@@ -34,9 +36,9 @@ class ExecutionEngine:
                         self.execute_order(order)
                 except Exception as e:
                     print(f"Error processing tick {tick} with strategy {strategy}: {e}")
-                except model.OrderError as e:
+                except models.OrderError as e:
                     print(f"Invalid order with strategy {strategy}: {e}")
-                except model.ExecutionError as e:
+                except models.ExecutionError as e:
                     print(f"Execution Failure with strategy {strategy}: {e}")
 
     def execute_order(self, order):
@@ -44,8 +46,10 @@ class ExecutionEngine:
             self.portfolio[order.symbol] = 0
 
         if order.side == "BUY":
-            self.portfolio[order.symbol] += order.quantity
+            self.portfolio[order.symbol] += order.quantity * order.price
+            self.cash -= order.quantity * order.price
         elif order.side == "SELL":
-            self.portfolio[order.symbol] -= order.quantity
+            self.portfolio[order.symbol] -= order.quantity * order.price
+            self.cash += order.quantity * order.price
 
         print(f"Executed {order.side} order for {order.quantity} of {order.symbol} at {order.price}. Current position: {self.portfolio[order.symbol]}")
