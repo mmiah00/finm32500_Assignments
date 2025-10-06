56ba1a07801fa3de0c2ffc97b5d103a63f51792b
mmiah <mmiah@bu.edu>
2025-10-05 20:54:50 -0500

    working on writing notebook. needs to be tested

--- Assignment2/Strategy.py
+++ Assignment2/Strategy.py
@@ -4,7 +4,8 @@ class Strategy:
     def __init__(self, initial_cash=1_000_000):
         self.initial_cash = initial_cash
         self.cash = initial_cash
-        self.portfolio = {}  # ticker -> shares held
+        self.signals = {} # key = ticker, value = pd.Series of buy/sell signals (-1 = sell, 0 = no action, 1 = buy)
+        self.portfolio = {}  # key = ticker, value = num shares held of ticker
         self.history = []    # track (date, portfolio_value, cash, holdings_value, pnl)
 
     def run(self, price_data):
@@ -31,17 +32,20 @@ class Strategy:
         if cost <= self.cash: 
             self.cash -= cost 
             self.portfolio[ticker] = self.portfolio.get(ticker, 0) + amount
+            self._log_signal(ticker, date, 1) # 1 = buy signal 
         else: 
-            raise ValueError(f"Not enough cash to buy {amount} shares of {ticker}. Cash amount: {self.cash}. Purchase attempted: {cost}.") 
+            self._log_signal(ticker, date, 0) # 0 = no action 
+            # raise ValueError(f"Not enough cash to buy {amount} shares of {ticker}. Cash amount: {self.cash}. Purchase attempted: {cost}.") 
 
-    def get_results(self):
-        """Return portfolio performance as a DataFrame."""
-        return pd.DataFrame(self.history, columns=["Date", "Portfolio_Value", "Cash", "HoldingsValue", "PnL"].set_index("Date"))
+    def _log_signal(self, ticker, date, signal_value):
+        if ticker not in self.signals:
+            self.signals[ticker] = pd.Series(dtype=float)
+        self.signals[ticker].at[date] = signal_value
 
     def get_results(self):
         """Return portfolio performance as a DataFrame."""
         df = pd.DataFrame(self.history, columns=["Date", "Portfolio_Value", "Cash", "HoldingsValue", "PnL"]).set_index("Date")
 
         df["Cumulative_PnL"] = df["PnL"].cumsum()
-        
+
         return df
