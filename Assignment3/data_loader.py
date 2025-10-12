e3b0d303668faac254d86574abe50ba3c5e6a8d5
mmiah <mmiah@bu.edu>
2025-10-11 20:52:43 -0500

    fixed bug

--- Assignment3/data_loader.py
+++ Assignment3/data_loader.py
@@ -4,28 +4,26 @@ from collections import defaultdict
 from dataclasses import dataclass
 
 df = pd.read_csv ('market_data.csv')
-print(df.head() )
+
 
 @dataclass(frozen=True)
 class MarketDataPoint:
-    def __init__ (self, timestamp, symbol, price): 
-        # timestamp: datetime
-        # symbol: str
-        # price: float
-        self.timestamp = timestamp 
-        self.symbol = symbol 
-        self.price = price 
+    timestamp: str
+    symbol: str
+    price: float
     
 MDPs = []  # list of market data point objects generated from market data csv 
-MDPs_by_ticker = defaultdict(list)
+# MDPs_by_ticker = defaultdict(list)
 
 for index, row in df.iterrows():
     try: 
         mdp = MarketDataPoint (row['timestamp'], row['symbol'], row['price'])
         MDPs.append(mdp) 
-        MDPs_by_ticker[row['symbol']].append(mdp)
-    except: 
-        print (f"Had issues loading this row from market_data.csv:\n{row}")
+        # print(f"Added mdp. Timestamp : {row['timestamp']} | Symbol : {row['symbol']} | Price : {row['price']}")
+        # MDPs_by_ticker[row['symbol']].append(mdp)
+    except Exception as e: 
+        print(e)
+        print (f"Had issues loading this row from market_data.csv:\n{row}\n")
 
 # O(N) space complexity, given that you are reading in each row from the market_data.csv file and saving them as a MarketDataPoint object. 
 
