20fb870ffbcafc274eb3d2795f4db5bbc1529255
mmiah <mmiah@bu.edu>
2025-10-11 20:45:36 -0500

    tested loader, seems to work

--- Assignment3/data_loader.py
+++ Assignment3/data_loader.py
@@ -1,7 +1,10 @@
 import sys
 import pandas as pd 
+from collections import defaultdict
+from dataclasses import dataclass
 
 df = pd.read_csv ('market_data.csv')
+print(df.head() )
 
 @dataclass(frozen=True)
 class MarketDataPoint:
@@ -14,12 +17,15 @@ class MarketDataPoint:
         self.price = price 
     
 MDPs = []  # list of market data point objects generated from market data csv 
+MDPs_by_ticker = defaultdict(list)
 
-for row in df: 
+for index, row in df.iterrows():
     try: 
         mdp = MarketDataPoint (row['timestamp'], row['symbol'], row['price'])
         MDPs.append(mdp) 
+        MDPs_by_ticker[row['symbol']].append(mdp)
     except: 
         print (f"Had issues loading this row from market_data.csv:\n{row}")
 
 # O(N) space complexity, given that you are reading in each row from the market_data.csv file and saving them as a MarketDataPoint object. 
+
