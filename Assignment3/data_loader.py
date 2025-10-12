6f9b4ff27d97529eb4cf09348953a009b9df451e
mmiah <mmiah@bu.edu>
2025-10-12 13:07:48 -0500

    adjusted data loader to account for new csv file

--- Assignment3/data_loader.py
+++ Assignment3/data_loader.py
@@ -14,12 +14,13 @@ class MarketDataPoint:
     price: float
     
 MDPs = []  # list of market data point objects generated from market data csv 
-# MDPs_by_ticker = defaultdict(list)
+MDPs_by_ticker = defaultdict(list) # dictionary with key = ticker, value = list of MDPs for that ticker 
 
 for index, row in df.iterrows():
     try: 
         mdp = MarketDataPoint (pd.to_datetime(row['timestamp']), row['symbol'], row['price'])
         MDPs.append(mdp) 
+        MDPs_by_ticker[row['symbol']].append(mdp) 
     except Exception as e: 
         print(e)
         print (f"Had issues loading this row from market_data.csv:\n{row}\n")
