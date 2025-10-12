3c5b9ac50dd7ed7e469ec74fceb37160338a2ffb
mmiah <mmiah@bu.edu>
2025-10-11 21:56:01 -0500

    wrote naive ma strategy: ;

--- Assignment3/NaiveMovingAverageStrategy.py
+++ Assignment3/NaiveMovingAverageStrategy.py
@@ -4,29 +4,32 @@ from Strategy import Strategy
 
 class NaiveMovingAverageStrategy(Strategy):
 
-    def __init__ (self, initial_cash=1000000): 
-        """
-        Moving Average Strategy
-        - Buy if 20-day MA > 50-day MA 
-        
-        Args:
-            initial_cash: starting cash
-        """
-        super().__init__(initial_cash)
+    def __init__ (self, window_size=20): 
+        self.window_size = window_size 
+        self.prices = []
     
-    def run(self, price_data): 
-        # print ("Running Moving Average Strategy...")
+    def generate_signals(self, mdps): 
+        signals = [] 
 
-        for ticker, price_series in price_data.items(): 
-            ma20 = price_series.rolling(window=20).mean()
-            ma50 = price_series.rolling(window=50).mean()
-            buy_signals = ma20 > ma50  # boolean Series
-            dates = price_series.index
+        for mdp in mdps: 
+            if len(prices) < self.window_size: 
+                self.prices.append(mdp.price)
+                signals.append('HOLD')
+            else: 
+                avg = np.mean(signals[-self.window_size:]) # last k elements, which is the size of the window 
+                if mdp.price > avg: 
+                    signals.append('BUY')
+                elif mdp.price < avg: 
+                    signals.append('SELL')
+                else: 
+                    signals.append('HOLD')
+                
+        return signals 
 
-            buy_dates = price_series.index[buy_signals]
-            for date in buy_dates:
-                try:
-                    self._buy(ticker, price_series.loc[date], 1, date)
-                except Exception:
-                    pass
-                self._record(date, price_data)
+        # Time Complexity: O(N) -> This algorithm calculates the moving average for each market data point 
+        #                          in the data, so as the number of data points increases, the time it takes 
+        #                          to the run the algorithm increases as well as there are more data points 
+        #                          to process.  
+
+        # Space Complexity: O(N) -> This algorithm stores the moving averages for each data point in an array, 
+        #                           which takes up O(N) space.  
--- Assignment3/data_loader.py
+++ Assignment3/data_loader.py
@@ -20,8 +20,6 @@ for index, row in df.iterrows():
     try: 
         mdp = MarketDataPoint (pd.to_datetime(row['timestamp']), row['symbol'], row['price'])
         MDPs.append(mdp) 
-        # print(f"Added mdp. Timestamp : {row['timestamp']} | Symbol : {row['symbol']} | Price : {row['price']}")
-        # MDPs_by_ticker[row['symbol']].append(mdp)
     except Exception as e: 
         print(e)
         print (f"Had issues loading this row from market_data.csv:\n{row}\n")
