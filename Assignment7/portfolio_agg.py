75e56137dcdd59e6d861f2207861adf84075c1a3
mmiah <mmiah@bu.edu>
2025-11-02 13:41:26 -0600

    working on part 4

--- Assignment7/portfolio_agg.py
+++ Assignment7/portfolio_agg.py
@@ -9,3 +9,26 @@ for position in data["positions"]:
     value = position["quantity"] * position["price"]
     print(value)
 
+# sequential 
+def calc_metrics_seq (df, symbol, qty=1):
+    '''
+    value = quantity × latest price
+    volatility = rolling standard deviation of returns
+    drawdown = maximum peak-to-trough loss
+    '''
+    latest_price = df[symbol].sort()[-1] 
+    df['value'] = qty * latest_price 
+
+    df['returns'] = df[symbol].pct_change() 
+    df['volatility'] = df['returns'].rolling(window=20).std()
+
+    cumulative_returns = (1 + df['returns']).cumprod() - 1
+    df['drawdown'] = (cumulative_returns - cumulative_returns.cummax()) / cumulative_returns.cummax()
+
+    return df
+
+calc_metrics_seq() 
+
+# parallel 
+
+
