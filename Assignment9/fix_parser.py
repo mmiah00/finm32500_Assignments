f9e86c3d74dba9364b322efad4e5d5ef12707377
mmiah <mmiah@bu.edu>
2025-11-18 01:40:24 -0600

    started writing parse

---
+++ Assignment9/fix_parser.py
@@ -0,0 +1,20 @@
+class FixParser:
+    def __init__(self, delimiter='|'): 
+        self.delimiter = delimiter
+
+    def parse(self, message):
+        fields = message.split(self.delimiter)
+        fix_dict = {}
+        for field in fields:
+            if '=' in field:
+                tag, value = field.split('=', 1)
+                fix_dict[tag] = value
+
+        if 'price' not in fix_dict  or 'side' not in fix_dict:
+            raise ValueError("Missing required fields: 'price' and 'side'")
+            
+        return fix_dict
+
+if __name__ == "__main__":
+    msg = "8=FIX.4.2|35=D|55=AAPL|54=1|38=100|40=2|10=128"
+    print(FixParser().parse(msg))
