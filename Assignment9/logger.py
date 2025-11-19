d1c8505948ff702978973304c5d0f60a8a8a7f57
mmiah <mmiah@bu.edu>
2025-11-18 18:33:24 -0600

    working on logger

--- Assignment9/logger.py
+++ Assignment9/logger.py
@@ -2,11 +2,26 @@ from datetime import datetime
 import json
 
 class Logger:
+    _instance = None
+
     def __init__(self, path="events.json"):
-        pass
+        self.path = path
+        self.events = []
+    
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
     
     def log(self, event_type, data):
-        pass
+        event = {
+            "timestamp": datetime.utcnow().isoformat() + "Z",
+            "type": event_type,
+            "data": data
+        }
+        self.events.append(event)
 
     def save(self):
-        pass
+        # save to events.json 
+        with open(self.path, 'w') as f:
+            json.dump(self.events, f, indent=4)
