8197748d6103b196b52e71f831abf164422e1cc5
ericbeechen <ebeechen@uchicago.edu>
2025-11-18 18:12:52 -0600

    Momentum

--- EndToEnd/main.py
+++ EndToEnd/main.py
@@ -1,19 +1,24 @@
 from PriceLoader import PriceLoader
 from DataCleaner import Cleaner
+from Momentum import Momentum
+import pandas as pd
 import os
 
-"""
-Get all price data using PriceLoader 
-"""
+
 # print(os.getcwd())
-loader = PriceLoader()
-ticks = loader.load_tickers() 
+# loader = PriceLoader()
+# ticks = loader.load_tickers() 
 # loader.download_ticker_prices()  
-loader.get_select_ticker_data(loader.tickers)
-directory = '/Users/ericbeechen/Documents/GitHub/finm32500_Assignments/EndToEnd/data/'
+# loader.get_select_ticker_data(loader.tickers)
+base = '/Users/ericbeechen/Documents/GitHub/finm32500_Assignments/EndToEnd/'
+directory = f'{base}data/'
 names = []
 for entry in os.scandir(directory):
     names.append(entry.name)
-for name in names:
-    cleaner = Cleaner(name)
+# for name in names:
+#     cleaner = Cleaner(name)
 
+df = pd.read_csv(f'{base}cleaned/{names[0]}')
+momentum = Momentum(df)
+results = momentum.run()
+print(results)
