3f3e4905fa8a5706bbf5db7191650c1eb6d11061
ericbeechen <ebeechen@uchicago.edu>
2025-11-23 16:22:23 -0600

    Alpaca sucks

--- EndToEnd/Alpaca.py
+++ EndToEnd/Alpaca.py
@@ -1,5 +1,10 @@
 import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import REST, TimeFrame, TimeFrameUnit
 
-api = tradeapi.REST('PKS3ESRPJZHGHE3FY4BYP77JI6', 'A3JoYsEQ3sPJcFRHDQfSQgv5zepQPE7Muaapz9c4NRYa', 'https://paper-api.alpaca.markets/v2')
+api = tradeapi.REST(key_id='PKS3ESRPJZHGHE3FY4BYP77JI6', secret_key='A3JoYsEQ3sPJcFRHDQfSQgv5zepQPE7Muaapz9c4NRYa',base_url='https://paper-api.alpaca.markets', api_version='v2')
 
-data = api.get_barset('AAPL', '1Min', limit=1).df['AAPL']
+# api = REST()
+
+df = api.get_bars("AAPL", TimeFrame(45, TimeFrameUnit.Minute), "2021-06-08", "2021-06-08", adjustment='raw').df
+
+print(df)
