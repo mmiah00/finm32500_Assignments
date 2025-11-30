8fdb9fe5118d04bfb2cbbdb8611c5a53ac884f95
ericbeechen <ebeechen@uchicago.edu>
2025-11-30 09:24:24 -0600

    Fixed API

--- EndToEnd/Alpaca.py
+++ EndToEnd/Alpaca.py
@@ -1,7 +1,11 @@
 import alpaca_trade_api as tradeapi
 from alpaca_trade_api.rest import REST, TimeFrame, TimeFrameUnit
+from dotenv import load_dotenv
+import os
 
-api = tradeapi.REST(key_id='PKS3ESRPJZHGHE3FY4BYP77JI6', secret_key='A3JoYsEQ3sPJcFRHDQfSQgv5zepQPE7Muaapz9c4NRYa',base_url='https://paper-api.alpaca.markets', api_version='v2')
+load_dotenv()
+
+api = tradeapi.REST(key_id=os.getenv('KEY_ID'), secret_key=os.getenv('SECRET_KEY'),base_url='https://paper-api.alpaca.markets', api_version='v2')
 
 df = api.get_bars("AAPL", TimeFrame(45, TimeFrameUnit.Minute), "2021-06-08", "2021-06-08", adjustment='raw').df
 
